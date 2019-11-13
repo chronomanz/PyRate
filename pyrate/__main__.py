30523b2a6d66bd450a1952b29532ff34e8f88a3e
sheecegardezi <sheecegardezi@gmail.com>
2019-11-13 18:26:20 +1100

    rm overloaded config methods

--- pyrate/__main__.py
+++ pyrate/__main__.py
@@ -21,19 +21,19 @@ import logging
 import argparse
 from argparse import RawTextHelpFormatter
 from pyrate.core import config as cf
-from pyrate import (conv2tif, prepifg, process, merge)
-from pyrate import CONV2TIF, PREPIFG, PROCESS, MERGE # Step names
+from pyrate import conv2tif, prepifg, process, merge
 from pyrate.core import pyratelog
 import time
 
 log = logging.getLogger(__name__)
+
 
 def conv2tif_handler(config_file):
     """
     Convert interferograms to geotiff.
     """
     config_file = os.path.abspath(config_file)
-    params = cf.get_config_params(config_file, step=CONV2TIF)
+    params = cf.get_config_params(config_file)
     conv2tif.main(params)
 
 
@@ -42,7 +42,7 @@ def prepifg_handler(config_file):
     Perform multilooking and cropping on geotiffs.
     """
     config_file = os.path.abspath(config_file)
-    params = cf.get_config_params(config_file, step=PREPIFG)
+    params = cf.get_config_params(config_file)
     prepifg.main(params)
 
 
@@ -51,8 +51,8 @@ def process_handler(config_file, rows, cols):
     Time series and linear rate computation.
     """
     config_file = os.path.abspath(config_file)
-    _, dest_paths, params = cf.get_ifg_paths(config_file, step=PROCESS)
-    process.process_ifgs(sorted(dest_paths), params, rows, cols)
+    params = cf.get_config_params(config_file)
+    process.main(params, rows, cols)
 
 
 def merge_handler(config_file, rows, cols):
@@ -60,10 +60,11 @@ def merge_handler(config_file, rows, cols):
     Reassemble computed tiles and save as geotiffs.
     """
     config_file = os.path.abspath(config_file)
-    _, _, params = cf.get_ifg_paths(config_file, step=MERGE)
+    params = cf.get_config_params(config_file)
     merge.main(params, rows, cols)
 
-CLI_DESC = """
+
+CLI_DESCRIPTION = """
 PyRate workflow: 
 
     Step 1: conv2tif
@@ -75,11 +76,12 @@ Refer to https://geoscienceaustralia.github.io/PyRate/usage.html for
 more details.
 """
 
+
 def main():
     start_time = time.time()
     log.debug("Starting PyRate")
 
-    parser = argparse.ArgumentParser(prog='pyrate', description=CLI_DESC, add_help=True,  formatter_class=RawTextHelpFormatter)
+    parser = argparse.ArgumentParser(prog='pyrate', description=CLI_DESCRIPTION, add_help=True, formatter_class=RawTextHelpFormatter)
     parser.add_argument('-v', '--verbosity', type=str, default='INFO', choices=['DEBUG', 'INFO', 'WARNING', 'ERROR'], help="Increase output verbosity")
 
     subparsers = parser.add_subparsers(dest='command')
@@ -87,7 +89,7 @@ def main():
 
     # create the parser for the "conv2tif" command
     parser_conv2tif = subparsers.add_parser('conv2tif', help='Convert interferograms to geotiff.', add_help=True)
-    parser_conv2tif.add_argument('-f', '--config_file', action="store", type=str, default=None,  help="Pass configuration file", required=True)
+    parser_conv2tif.add_argument('-f', '--config_file', action="store", type=str, default=None, help="Pass configuration file", required=True)
 
     # create the parser for the "prepifg" command
     parser_prepifg = subparsers.add_parser('prepifg', help='Perform multilooking and cropping on geotiffs.', add_help=True)
@@ -98,7 +100,7 @@ def main():
     parser_process.add_argument('-f', '--config_file', action="store", type=str, default=None, help="Pass configuration file", required=True)
     parser_process.add_argument('-r', '--rows', type=int, required=False, default=1, help="divide ifgs into this many rows. Must be same as number of rows used previously in main workflow.")
     parser_process.add_argument('-c', '--cols', type=int, required=False, default=1, help="divide ifgs into this many columns. Must be same as number of cols used previously in main workflow.")
- 
+
     # create the parser for the "merge" command
     parser_merge = subparsers.add_parser('merge', help="Reassemble computed tiles and save as geotiffs.", add_help=True)
     parser_merge.add_argument('-f', '--config_file', action="store", type=str, default=None, help="Pass configuration file", required=False)
@@ -106,7 +108,6 @@ def main():
     parser_merge.add_argument('-c', '--cols', type=int, required=False, default=1, help="divide ifgs into this many columns. Must be same as number of cols used previously in main workflow.")
 
     args = parser.parse_args()
-
     log.debug("Arguments supplied at command line: ")
     log.debug(args)
 
