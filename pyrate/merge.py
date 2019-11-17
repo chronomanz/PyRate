2d9983afbe24f0d6b2a2eadb35230630dbf980bd
sheecegardezi <sheecegardezi@gmail.com>
2019-11-18 02:50:11 +1100

    update merge
    - rm dependency on mpi
    - use build-in multiprocessing module 
    - rm spaces

--- pyrate/merge.py
+++ pyrate/merge.py
@@ -25,15 +25,18 @@ import numpy as np
 from osgeo import gdal
 import subprocess
 
-from pyrate.core import shared, ifgconstants as ifc, mpiops, config as cf
+import pyrate.configuration
+import pyrate.constants
+import pyrate.configuration
+import pyrate.core.shared
+import pyrate.process
+from pyrate.core import shared, ifgconstants as ifc
 from pyrate.core.shared import PrereadIfg
-from pyrate.configuration import Configuration
+from pyrate.configuration import TMPDIR
 
 gdal.SetCacheMax(64)
 log = logging.getLogger(__name__)
 
-# Constants
-MASTER_PROCESS = 0
 
 def create_png_from_tif(output_folder_path):
 
@@ -104,18 +107,20 @@ def create_png_from_tif(output_folder_path):
     output_png_path = os.path.join(output_folder_path, "linrate.png")
     subprocess.check_call(["gdaldem", "color-relief", "-of", "PNG", input_tif_path, "-alpha", color_map_path, output_png_path, "-nearest_color_entry"])
 
-def main(params, rows, cols):
+
+def main(params):
     """
     PyRate merge main function. Assembles product tiles in to
     single geotiff files
     """
     # setup paths
+    rows, cols = params["SUB_ROW"], params["SUB_COL"]
     _merge_linrate(rows, cols, params)
-    if params[cf.TIME_SERIES_CAL]:
+    if params[pyrate.constants.TIME_SERIES_CAL]:
         _merge_timeseries(rows, cols, params)
 
     log.info('Start creating quicklook results.')
-    output_folder_path = os.path.dirname(params["tmpdir"])
+    output_folder_path = os.path.dirname(params[TMPDIR])
     create_png_from_tif(output_folder_path)
     log.info('Finished creating quick look results.')
 
@@ -126,34 +131,24 @@ def _merge_linrate(rows, cols, params):
     """
     # pylint: disable=expression-not-assigned
     # setup paths
-    xlks, _, crop = cf.transform_params(params)
+    xlks, _, crop = params["IFG_LKSX"], params["IFG_LKSY"], params["IFG_CROP_OPT"]
 
-    config_file = params["config_file_path"]
-    configration_settings = Configuration(config_file)
-    base_unw_paths = configration_settings.base_interferogram_paths
-
+    base_unw_paths = params["BASE_INTERFEROGRAM_PATHS"]
 
     if "tif" in base_unw_paths[0].split(".")[1]:
-        dest_tifs = cf.get_dest_paths(base_unw_paths, crop, params, xlks)
+        dest_tifs = pyrate.configuration.get_dest_paths(base_unw_paths, crop, params, xlks)
         for i, dest_tif in enumerate(dest_tifs):
             dest_tifs[i] = dest_tif.replace("_tif","")
     else:
-        dest_tifs = cf.get_dest_paths(base_unw_paths, crop, params, xlks)
+        dest_tifs = pyrate.configuration.get_dest_paths(base_unw_paths, crop, params, xlks)
 
     # load previously saved prepread_ifgs dict
-    preread_ifgs_file = join(params[cf.TMPDIR], 'preread_ifgs.pk')
+    preread_ifgs_file = join(params[pyrate.constants.TMPDIR], 'preread_ifgs.pk')
     ifgs = cp.load(open(preread_ifgs_file, 'rb'))
-    tiles = shared.get_tiles(dest_tifs[0], rows, cols)
+    tiles = pyrate.process.get_tiles(dest_tifs[0], rows, cols)
 
     # linrate aggregation
-    if mpiops.size >= 3:
-        [_save_linrate(ifgs, params, tiles, out_type=t)
-         for i, t in enumerate(['linrate', 'linerror', 'linsamples'])
-         if i == mpiops.rank]
-    else:
-        if mpiops.rank == MASTER_PROCESS:
-            [_save_linrate(ifgs, params, tiles, out_type=t)
-             for t in ['linrate', 'linerror', 'linsamples']]
+    [_save_linrate(ifgs, params, tiles, out_type=t) for t in ['linrate', 'linerror', 'linsamples']]
 
 
 def _save_linrate(ifgs_dict, params, tiles, out_type):
@@ -164,7 +159,7 @@ def _save_linrate(ifgs_dict, params, tiles, out_type):
     gt, md, wkt = ifgs_dict['gt'], ifgs_dict['md'], ifgs_dict['wkt']
     epochlist = ifgs_dict['epochlist']
     ifgs = [v for v in ifgs_dict.values() if isinstance(v, PrereadIfg)]
-    dest = os.path.join(params[cf.OUT_DIR], out_type + ".tif")
+    dest = os.path.join(params[pyrate.constants.OUT_DIR], out_type + ".tif")
     md[ifc.EPOCH_DATE] = epochlist.dates
     if out_type == 'linrate':
         md[ifc.DATA_TYPE] = ifc.LINRATE
@@ -175,11 +170,11 @@ def _save_linrate(ifgs_dict, params, tiles, out_type):
 
     rate = np.zeros(shape=ifgs[0].shape, dtype=np.float32)
     for t in tiles:
-        rate_file = os.path.join(params[cf.TMPDIR], out_type + '_{}.npy'.format(t.index))
+        rate_file = os.path.join(params[pyrate.constants.TMPDIR], out_type + '_{}.npy'.format(t.index))
         rate_tile = np.load(file=rate_file)
         rate[t.top_left_y:t.bottom_right_y, t.top_left_x:t.bottom_right_x] = rate_tile
     shared.write_output_geotiff(md, gt, wkt, rate, dest, np.nan)
-    npy_rate_file = os.path.join(params[cf.OUT_DIR], out_type + '.npy')
+    npy_rate_file = os.path.join(params[pyrate.constants.OUT_DIR], out_type + '.npy')
     np.save(file=npy_rate_file, arr=rate)
 
     log.info('Finished PyRate merging {}'.format(out_type))
@@ -190,20 +185,18 @@ def _merge_timeseries(rows, cols, params):
     Merge time series output
     """
     # pylint: disable=too-many-locals
-    xlks, _, crop = cf.transform_params(params)
+    xlks, _, crop = params["IFG_LKSX"], params["IFG_LKSY"], params["IFG_CROP_OPT"]
 
-    config_file = params["config_file_path"]
-    configration_settings = Configuration(config_file)
-    base_unw_paths = configration_settings.base_interferogram_paths
+    base_unw_paths = params["BASE_INTERFEROGRAM_PATHS"]
 
     if "tif" in base_unw_paths[0].split(".")[1]:
-        dest_tifs = cf.get_dest_paths(base_unw_paths, crop, params, xlks)
+        dest_tifs = pyrate.configuration.get_dest_paths(base_unw_paths, crop, params, xlks)
         for i, dest_tif in enumerate(dest_tifs):
             dest_tifs[i] = dest_tif.replace("_tif", "")
     else:
-        dest_tifs = cf.get_dest_paths(base_unw_paths, crop, params, xlks)
+        dest_tifs = pyrate.configuration.get_dest_paths(base_unw_paths, crop, params, xlks)
 
-    output_dir = params[cf.TMPDIR]
+    output_dir = params[pyrate.constants.TMPDIR]
 
     # load previously saved prepread_ifgs dict
     preread_ifgs_file = join(output_dir, 'preread_ifgs.pk')
@@ -214,7 +207,7 @@ def _merge_timeseries(rows, cols, params):
     epochlist = ifgs['epochlist']
     ifgs = [v for v in ifgs.values() if isinstance(v, PrereadIfg)]
 
-    tiles = shared.get_tiles(dest_tifs[0], rows, cols)
+    tiles = pyrate.process.get_tiles(dest_tifs[0], rows, cols)
 
     # load the first tsincr file to determine the number of time series tifs
     tsincr_file = os.path.join(output_dir, 'tsincr_0.npy')
@@ -224,14 +217,13 @@ def _merge_timeseries(rows, cols, params):
     no_ts_tifs = tsincr.shape[2]
     # we create 2 x no_ts_tifs as we are splitting tsincr and tscuml
     # to all processes.
-    process_tifs = mpiops.array_split(range(2 * no_ts_tifs))
+    process_tifs = range(2 * no_ts_tifs)
 
     # depending on nvelpar, this will not fit in memory
     # e.g. nvelpar=100, nrows=10000, ncols=10000, 32bit floats need 40GB memory
     # 32 * 100 * 10000 * 10000 / 8 bytes = 4e10 bytes = 40 GB
     # the double for loop helps us overcome the memory limit
-    log.info('process {} will write {} ts (incr/cuml) tifs of '
-             'total {}'.format(mpiops.rank, len(process_tifs), no_ts_tifs * 2))
+    log.info('process {} will write {} ts (incr/cuml) tifs of ''total {}'.format(1, len(process_tifs), no_ts_tifs * 2))
     for i in process_tifs:
         tscum_g = np.empty(shape=ifgs[0].shape, dtype=np.float32)
         if i < no_ts_tifs:
@@ -240,9 +232,7 @@ def _merge_timeseries(rows, cols, params):
             md[ifc.EPOCH_DATE] = epochlist.dates[i + 1]
             # sequence position; first time slice is #0
             md['SEQUENCE_POSITION'] = i+1
-            dest = os.path.join(params[cf.OUT_DIR],
-                                'tscuml' + "_" +
-                                str(epochlist.dates[i + 1]) + ".tif")
+            dest = os.path.join(params[pyrate.constants.OUT_DIR],'tscuml' + "_" + str(epochlist.dates[i + 1]) + ".tif")
             md[ifc.DATA_TYPE] = ifc.CUML
             shared.write_output_geotiff(md, gt, wkt, tscum_g, dest, np.nan)
         else:
@@ -253,13 +243,10 @@ def _merge_timeseries(rows, cols, params):
             md[ifc.EPOCH_DATE] = epochlist.dates[i + 1]
             # sequence position; first time slice is #0
             md['SEQUENCE_POSITION'] = i+1
-            dest = os.path.join(params[cf.OUT_DIR],
-                                'tsincr' + "_" + str(
-                                    epochlist.dates[i + 1]) + ".tif")
+            dest = os.path.join(params[pyrate.constants.OUT_DIR],'tsincr' + "_" + str(epochlist.dates[i + 1]) + ".tif")
             md[ifc.DATA_TYPE] = ifc.INCR
             shared.write_output_geotiff(md, gt, wkt, tsincr_g, dest, np.nan)
-    log.info('process {} finished writing {} ts (incr/cuml) tifs of '
-             'total {}'.format(mpiops.rank, len(process_tifs), no_ts_tifs * 2))
+    log.info('process {} finished writing {} ts (incr/cuml) tifs of total {}'.format(1, len(process_tifs), no_ts_tifs * 2))
 
 
 def _assemble_tiles(i, n, tile, tsincr_g, output_dir, outtype):
